52591d6b3621ba024cab7e629ce3f65e01106940
Bram Klapwijk <bram.klapwijk00@outlook.com>
2020-11-17 10:21:36 +0100

    mongoose

--- lezerserver/app.js
+++ lezerserver/app.js
@@ -1,11 +1,15 @@
+import api from 'routing/api';
+
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/api', api)
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    mongoose.connect('mongodb://localhost:27017/reader', {useNewUrlParser: true}, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
 })

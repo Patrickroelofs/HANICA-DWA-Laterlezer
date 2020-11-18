a1d7580723d5a6c0b133c9c0b882d2372933ee60
Patrick Roelofs <contact@patrickroelofs.com>
2020-11-18 13:35:42 +0100

    add forms

--- lezerapp/src/login/Login.jsx
+++ lezerapp/src/login/Login.jsx
@@ -3,7 +3,29 @@ import React from 'react';
 // TODO: Login functionality
 function Login() {
   return (
-    <h1>Login</h1>
+    <>
+      <h1>Login</h1>
+
+      <form>
+        <div>
+          <label htmlFor="username">
+            Username
+            <input type="text" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="password">
+            Password
+            <input type="password" />
+          </label>
+        </div>
+
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </>
   );
 }
 
--- lezerapp/src/register/Register.jsx
+++ lezerapp/src/register/Register.jsx
@@ -3,7 +3,43 @@ import React from 'react';
 // TODO: Register functionality
 function Register() {
   return (
-    <h1>Register</h1>
+    <>
+      <h1>Register</h1>
+
+      <form>
+        <div>
+          <label htmlFor="firstname">
+            First Name
+            <input type="text" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="lastname">
+            Last Name
+            <input type="text" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="username">
+            Username
+            <input type="text" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="password">
+            Password
+            <input type="password" />
+          </label>
+        </div>
+
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </>
   );
 }
 

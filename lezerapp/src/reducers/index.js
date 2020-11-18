8eb5d4a92ff858abc80c04bde22a1d68a063a583
Patrick Roelofs <contact@patrickroelofs.com>
2020-11-18 21:18:51 +0100

    add redux toolkit

--- lezerapp/src/reducers/index.js
+++ lezerapp/src/reducers/index.js
@@ -1,7 +1,28 @@
-import { combineReducers } from 'redux';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-const RootReducer = combineReducers({
+// Logger logs all states after actions
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log('ACTION:', action.type, action);
+  const result = next(action);
+  console.log('STATE AFTER ACTION:', action.type, store.getState());
+  return result;
+};
 
+const initialState = {
+
+};
+
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [loggerMiddleware, ...getDefaultMiddleware()],
+  devTools: true,
 });
 
-export default RootReducer;
+export default store;

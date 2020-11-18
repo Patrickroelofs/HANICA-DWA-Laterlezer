8da12dd860507c98157bb01d1a6161b5117b047d
Sander Wiggers <sander.wiggers@student.hu.nl>
2020-11-18 13:22:47 +0100

    deleted empty seeder

--- lezerserver/seeders/user.seeder.js
+++
@@ -1,9 +0,0 @@
-const user = new User({
-    firstName: "test",
-    lastName: "test",
-    userName: "test",
-    password: "test",
-    email: "test@test.nl",
-    articles: [],
-    tags: [],
-})

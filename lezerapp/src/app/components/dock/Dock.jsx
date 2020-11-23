bf7cf680018e7ef0f592ac662ca1c7d23393061e
Patrick Roelofs <contact@patrickroelofs.com>
2020-11-23 12:04:27 +0100

    add logout button to app
    clears localstorage, pushes to login and reloads site for the state to actually get reset.

--- lezerapp/src/app/components/dock/Dock.jsx
+++ lezerapp/src/app/components/dock/Dock.jsx
@@ -1,8 +1,18 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './dock.scss';
 
 function Dock() {
+  const history = useHistory();
+
+  const onLogout = () => {
+    localStorage.clear();
+
+    history.push('/login');
+    window.location.reload();
+  };
+
   return (
     <section id="dock">
       <div>
@@ -10,6 +20,7 @@ function Dock() {
       </div>
 
       <div>
+        <button type="button" onClick={onLogout} className="text-center w-full">Logout</button>
         <button className="profile" type="button"><img alt="" src="http://placehold.it/48x48" /></button>
       </div>
     </section>

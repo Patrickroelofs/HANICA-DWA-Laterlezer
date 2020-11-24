5a4431685815abb9cf81785b90ba903a4ec5f600
Bram Klapwijk <bram.klapwijk00@outlook.com>
2020-11-24 11:05:26 +0100

    fix unneeded changes

--- lezerapp/src/app/components/newTag/NewTag.jsx
+++ lezerapp/src/app/components/newTag/NewTag.jsx
@@ -45,33 +45,31 @@ function NewTag() {
   }, []);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="tagTitle">
-          Tag title:
-          <input type="text" id="tagTitle" name="tagTitle" value={title} onChange={changeTitle} className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md l:text-l border-gray-300" />
-        </label>
-        <label htmlFor="showPicker">
-          Tag color:
-          <input
-            type="button"
-            id="showPicker"
-            value={color}
-            style={{ backgroundColor: color }}
-            onClick={changeShowPicker}
-            className="block w-full items-center px-3 py-1 mb-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
-          />
-        </label>
-        <input type="submit" value="Submit" className="block w-full items-center px-3 py-1 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none" />
-        {(showPicker) ? (
-          <ChromePicker
-            color={color}
-            onChange={handleChangeComplete}
-          />
-        ) : null}
-        <hr className="my-3" />
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="tagTitle">
+        Tag title:
+        <input type="text" id="tagTitle" name="tagTitle" value={title} onChange={changeTitle} className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md l:text-l border-gray-300" />
+      </label>
+      <label htmlFor="showPicker">
+        Tag color:
+        <input
+          type="button"
+          id="showPicker"
+          value={color}
+          style={{ backgroundColor: color }}
+          onClick={changeShowPicker}
+          className="block w-full items-center px-3 py-1 mb-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
+        />
+      </label>
+      <input type="submit" value="Submit" className="block w-full items-center px-3 py-1 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none" />
+      {(showPicker) ? (
+        <ChromePicker
+          color={color}
+          onChange={handleChangeComplete}
+        />
+      ) : null}
+      <hr className="my-3" />
+    </form>
   );
 }
 

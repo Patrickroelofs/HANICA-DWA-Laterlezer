e5fb8342c3280f7c0234b7e237ad9c4224cd1a78
Stan <stan.gerritsen@student.hu.nl>
2020-11-24 12:14:42 +0100

    added tags to artikel

--- lezerapp/src/app/components/reader/Reader.jsx
+++ lezerapp/src/app/components/reader/Reader.jsx
@@ -32,6 +32,9 @@ function Reader() {
         </a>
       </div>
       <h1 className="font-bold text-3xl pt-16 pb-4">{article.title}</h1>
+      <div className="articleTags pb-4">
+        { (article.tags) ? article.tags.map((tag) => <span className="mr-4" key={tag.id} style={{ color: tag.color }}>{tag.title}</span>) : <span>No tags found</span> }
+      </div>
       <small className="text-md italic pb-4 block">
         { article.author }
         ,&nbsp;
@@ -44,9 +47,6 @@ function Reader() {
       </div>
       {/* eslint-disable-next-line react/no-danger */}
       <div className="max-w-2xl m-auto mb-64 article" dangerouslySetInnerHTML={{ __html: article.html }} />
-      <div className="articleTags">
-        { (article.tags) ? article.tags.map((tag) => <li key={tag.id} style={{ color: tag.color }}>{tag.title}</li>) : <li>No tags found</li> }
-      </div>
     </div>
   );
 }

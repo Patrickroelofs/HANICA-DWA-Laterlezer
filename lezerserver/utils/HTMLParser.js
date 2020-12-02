d027dea05a6cc12bb5a42937d35e35aa0b0097af
Imre Boersma <imre.boersma@gmail.com>
2020-12-02 11:15:03 +0100

    added parser tests

--- lezerserver/utils/HTMLParser.js
+++ lezerserver/utils/HTMLParser.js
@@ -1,3 +1,19 @@
 const { parse } = require('@postlight/mercury-parser');
+const { JSDOM } = require('jsdom');
 
-module.exports = (url) => parse(url);
+exports.parseHTML = (url) => parse(url);
+
+exports.serializeHTML = (html) => {
+  const dom = new JSDOM(html);
+  dom.serialize();
+  const { document } = dom.window;
+
+  Array.from(document.getElementsByTagName('img')).forEach((img) => {
+    img.classList.add('lazy');
+    img.setAttribute('data-src', img.src);
+    img.removeAttribute('src');
+    img.removeAttribute('alt');
+    img.removeAttribute('srcset');
+  });
+  return dom.window.document.documentElement.outerHTML;
+};

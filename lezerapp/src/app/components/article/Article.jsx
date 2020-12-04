2a27ce5fe6ffad4f9af3a0d9764c05a540e38c5c
Imre Boersma <imre.boersma@gmail.com>
2020-12-04 15:50:36 +0100

    deselect tagPill

--- lezerapp/src/app/components/article/Article.jsx
+++ lezerapp/src/app/components/article/Article.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Link, NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Thumbnail from '../thumbnail/Thumbnail';
 import TagPill from '../tagPill/TagPill';
 import { setCurrentArticle } from '../../../store/articleSlice';
-import { setSelectedTags } from '../../../store/tagSlice';
+import { selectSelectedTags, setSelectedTags } from '../../../store/tagSlice';
 
 function Article({ article }) {
   const {
     _id, image, tags, title, description,
   } = article;
+  const selectedTags = useSelector(selectSelectedTags);
 
   const dispatch = useDispatch();
-  const clickHandler = (tag) => {
-    dispatch(setSelectedTags([tag.title]));
+
+  const selectTag = (tag) => {
+    let tagss = [...selectedTags];
+    if (!tagss.includes(tag.title)) {
+      tagss.push(tag.title);
+    } else {
+      tagss = selectedTags.filter((t) => t !== tag.title);
+    }
+    dispatch(setSelectedTags(tagss));
   };
   return (
-    <Link style={{}} to={`app/${_id}`} onClick={() => dispatch(setCurrentArticle(article))}>
+    <Link to={`app/${_id}`} onClick={() => dispatch(setCurrentArticle(article))}>
       <article className="grid grid-cols-4 mt-6">
         <Thumbnail url={image || 'https://placehold.it/125x100'} />
         <div className="col-span-3 ml-8">
           <div className="articleTags pb-2 text-xs overflow-x-hidden whitespace-nowrap overflow-ellipsis">
             { (tags) ? tags.map((tag) => (
-              <Link style={{}} key={tag.title} to="/app" onClick={() => clickHandler(tag)} value={tag}>
+              <NavLink key={tag.title} to="/app" onClick={() => selectTag(tag)} value={tag._id}>
                 <TagPill data={tag} />
-              </Link>
+              </NavLink>
             )) : <span>No tags found</span> }
           </div>
           <strong className="font-bold text-md">{ title }</strong>

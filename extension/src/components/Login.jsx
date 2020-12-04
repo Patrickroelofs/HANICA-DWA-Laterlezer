0a52f8922308788149bbaaf62574920c94ff7ecd
Patrick Roelofs <contact@patrickroelofs.com>
2020-12-04 20:53:44 +0100

    Automatically log the user in (on the extension) if they activate the extension on the app

--- extension/src/components/Login.jsx
+++ extension/src/components/Login.jsx
@@ -3,6 +3,20 @@ import React, { useState } from 'react';
 function Login({ setUser }) {
   const [name, setName] = useState('');
 
+  function receiveText(resultsArray) {
+    const usernameResult = JSON.parse(resultsArray[0]).username;
+    setName(usernameResult);
+    setUser(usernameResult);
+  }
+
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    if (tabs[0].selected === true && tabs[0].url === 'http://localhost:3001/app') {
+      chrome.tabs.executeScript({
+        code: 'JSON.parse(localStorage.getItem(\'persist:root\')).user',
+      }, receiveText);
+    }
+  });
+
   return (
     <div className="login">
       <input type="text" placeholder="Username" onChange={(e) => setName(e.target.value)} value={name} />

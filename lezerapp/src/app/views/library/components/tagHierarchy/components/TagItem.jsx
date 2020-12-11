17a8ad8c9f4354fe1f7bc33ae184041276b6a073
Sander Wiggers <sander.wiggers@student.hu.nl>
2020-12-11 13:03:10 +0100

    fixed bug

--- lezerapp/src/app/views/library/components/tagHierarchy/components/TagItem.jsx
+++ lezerapp/src/app/views/library/components/tagHierarchy/components/TagItem.jsx
@@ -8,7 +8,7 @@ export default ({
   <li key={tag._id} className={`transform hover:scale-105 hover:bg-gray-100 motion-reduce:transform-none transition-transform rounded-lg my-1 ${selectedTags.includes(tag.title) && 'bg-gray-200'}`}>
     <label htmlFor={`Tag-${tag._id}`} className="group px-4 py-2 block cursor-pointer">
       <span className="w-6 h-6 inline-block rounded-full align-middle mr-2" style={{ background: tag.color, color: setContrast(tag.color) }} />
-      <input onChange={selectTag} value={tag.title} hidden type="checkbox" id={`Tag-${tag._id}`} />
+      <input onChange={selectTag} value={tag.title} hidden type="checkbox" id={`Tag-${tag._id}`} checked={selectedTags.includes(tag.title)} />
       {tag.title}
       <button className="focus:outline-none opacity-0 group-hover:opacity-100 w-6 h-6 float-right bg-gray-200 rounded-full" type="submit" onClick={() => handleClick(tag)}>
         <AddIcon fontSize="small" />

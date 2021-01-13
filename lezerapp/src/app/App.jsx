5caf2419cb20a949ecfd15b308ef96090c240ea6
Patrick Roelofs <contact@patrickroelofs.com>
2021-01-13 12:40:22 +0100

    combine navigation

--- lezerapp/src/app/App.jsx
+++ lezerapp/src/app/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import Dock from './views/sharedcomponents/dock/Dock';
-import Nav from './views/sharedcomponents/nav/Nav';
 import Articles from './views/library/Articles';
-import { selectUsername, selectProfilePicture } from '../store/userSlice';
+import { selectUsername } from '../store/userSlice';
 import AddArticle from './views/sharedcomponents/addArticle/AddArticle';
 
 function App() {
   const username = useSelector(selectUsername);
-  const profilePicture = useSelector(selectProfilePicture);
 
   axios.interceptors.request.use((config) => {
     if (username) {
@@ -20,28 +17,12 @@ function App() {
   });
 
   return (
-    <div className="min-h-screen bg-gray-50 font-serif text-sm">
-      <div className="min-h-full md:grid grid-cols-4">
-        <nav className="col-span-1">
-          <div className="grid grid-cols-5 min-h-full">
-            <div className="col-span-1 bg-white relative top-0">
-              <Dock profilePicture={profilePicture} />
-            </div>
-            <div className="col-span-4 relative top-0">
-              <div className="min-h-screen sticky top-0 z-50">
-                <Nav />
-              </div>
-            </div>
-          </div>
-        </nav>
-        <main className="min-h-screen col-span-3 bg-white">
-          <AddArticle />
-          <div className="container max-w-5xl mx-auto p-16 pt-8 pb-0">
-            <Articles />
-          </div>
-        </main>
+    <>
+      <AddArticle />
+      <div className="container max-w-5xl mx-auto p-16 pt-8 pb-0">
+        <Articles />
       </div>
-    </div>
+    </>
   );
 }
 
--- lezerapp/src/router/Router.jsx
+++ lezerapp/src/router/Router.jsx
@@ -8,10 +8,13 @@ import Login from '../app/views/login/Login';
 import Register from '../app/views/register/Register';
 import Reader from '../app/views/reader/Reader';
 import App from '../app/App';
-import { selectUsername } from '../store/userSlice';
+import { selectUsername, selectProfilePicture } from '../store/userSlice';
+import Nav from '../app/views/sharedcomponents/nav/Nav';
+import Dock from '../app/views/sharedcomponents/dock/Dock';
 
 function Router() {
   const username = useSelector(selectUsername);
+  const profilePicture = useSelector(selectProfilePicture);
 
   const checkLoggedIn = (comp) => (!username ? <Redirect to="/login" /> : comp);
   const checkLoggedOut = (comp) => (username ? <Redirect to="/app" /> : comp);
@@ -23,25 +26,38 @@ function Router() {
           {checkLoggedIn(<Redirect to="/app" />)}
         </Route>
 
-        <Route path="/login">
-          {checkLoggedOut(<Login />)}
-        </Route>
+        <Route path="/login">{checkLoggedOut(<Login />)}</Route>
 
-        <Route path="/register">
-          {checkLoggedOut(<Register />)}
-        </Route>
+        <Route path="/register">{checkLoggedOut(<Register />)}</Route>
 
-        <Route path="/app" exact>
-          {checkLoggedIn(<App />)}
-        </Route>
+        <div className="min-h-screen bg-gray-50 font-serif text-sm">
+          <div className="min-h-full md:grid grid-cols-4">
+            <nav className="col-span-1">
+              <div className="grid grid-cols-5 min-h-full">
+                <div className="col-span-1 bg-white relative top-0">
+                  <Dock profilePicture={profilePicture} />
+                </div>
+                <div className="col-span-4 relative top-0">
+                  <div className="min-h-screen sticky top-0 z-50">
+                    <Nav />
+                  </div>
+                </div>
+              </div>
+            </nav>
 
-        <Route path="/app/status/:status" exact>
-          {checkLoggedIn(<App />)}
-        </Route>
+            <main className="min-h-screen col-span-3 bg-white">
+              <Route path="/app" exact>
+                {checkLoggedIn(<App />)}
+              </Route>
 
-        <Route path="/app/:id">
-          {checkLoggedIn(<Reader />)}
-        </Route>
+              <Route path="/app/status/:status" exact>
+                {checkLoggedIn(<App />)}
+              </Route>
+
+              <Route exact path="/app/:id">{checkLoggedIn(<Reader />)}</Route>
+            </main>
+          </div>
+        </div>
       </Switch>
     </BrowserRouter>
   );

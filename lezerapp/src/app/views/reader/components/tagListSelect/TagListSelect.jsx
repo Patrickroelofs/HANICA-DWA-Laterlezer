d196791231d6bcdea7045448ac5f2e128b78e7e4
Bram Klapwijk <bram.klapwijk00@outlook.com>
2021-01-14 13:50:56 +0100

    add auto update to tree when tags are updated

--- lezerapp/src/app/views/reader/components/tagListSelect/TagListSelect.jsx
+++ lezerapp/src/app/views/reader/components/tagListSelect/TagListSelect.jsx
@@ -3,7 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentArticle, addTags } from '../../../../../store/articleSlice';
 import TagPill from '../../../sharedcomponents/tagPill/TagPill';
 import TagParent from './tagParent/TagParent';
-import { getTags, selectTags as selectTagsStore } from '../../../../../store/tagSlice';
+import { getTags, selectTags as selectTagsStore, setSelectedTags as setSelectedTagsStore } from '../../../../../store/tagSlice';
+import TopDown from '../../../../../store/Classes/TopDown';
 
 function TagListSelect() {
   const article = useSelector(selectCurrentArticle);
@@ -17,28 +18,19 @@ function TagListSelect() {
 
   const selectTags = (selTags) => {
     if (isSelected(selTags[0])) {
-      const childrenIds = (tag) => tag.children.map((t) => [
-        t._id,
-        ...childrenIds(t),
-      ]).flat();
-      const childIds = childrenIds(selTags[0]);
-      const parentIds = selTags.filter((tag) => {
-        const hasSelectedChild = childrenIds(tag).filter((id) => {
-          if (!selTags.find((selTag) => id === selTag._id) && !childIds.includes(id)) {
-            return selectedTags.find((selTag) => id === selTag._id.toString());
-          }
-          return false;
-        });
-        return hasSelectedChild.length === 0;
-      }).map((tag) => tag._id.toString());
-      const ids = [...childIds, ...parentIds];
-      setSelectedTags(selectedTags.filter((t) => !ids.includes(t._id.toString())));
+      const model = new TopDown(tags, selectedTags);
+      setSelectedTags(model.deselectTag(selTags[0]));
     } else {
       setSelectedTags([
         ...selectedTags,
         ...selTags.filter((tag) => !isSelected(tag)),
       ]);
     }
+  };
+
+  const submitTags = () => {
+    dispatch(setSelectedTagsStore(selectedTags));
+    dispatch(addTags(article._id, selectedTags));
   };
 
   useEffect(() => {
@@ -71,7 +63,7 @@ function TagListSelect() {
       <button
         id="saveTagsToArticle"
         type="submit"
-        onClick={() => dispatch(addTags(article._id, selectedTags))}
+        onClick={submitTags}
         className="inline-block ml-2 h-9 items-center px-3 py-1 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none"
       >
         Save
